eb816f220c47cb64e50b84d93c89eefc04bb0f83
maximillianf22 <dgse.informatica@gmail.com>
2021-02-22 02:21:22 -0500

    Updates

---
+++ precache-manifest.334b884136015b70692bbc3c12bd2d30.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ab38575f9c8d18d0455a885c1ddb4b",
+    "url": "/hushpuppies/index.html"
+  },
+  {
+    "revision": "3dae95413655b1f8d16d",
+    "url": "/hushpuppies/static/css/2.0be0be60.chunk.css"
+  },
+  {
+    "revision": "baf51845c3fa7c3ac243",
+    "url": "/hushpuppies/static/css/main.627a0eb0.chunk.css"
+  },
+  {
+    "revision": "3dae95413655b1f8d16d",
+    "url": "/hushpuppies/static/js/2.6600e020.chunk.js"
+  },
+  {
+    "revision": "f5fc2541aa70c66bed1d80d3f2a13057",
+    "url": "/hushpuppies/static/js/2.6600e020.chunk.js.LICENSE"
+  },
+  {
+    "revision": "baf51845c3fa7c3ac243",
+    "url": "/hushpuppies/static/js/main.ca76909b.chunk.js"
+  },
+  {
+    "revision": "74a2da25f63b7a598415",
+    "url": "/hushpuppies/static/js/runtime-main.861368f5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/hushpuppies/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/hushpuppies/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/hushpuppies/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/hushpuppies/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/hushpuppies/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/hushpuppies/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/hushpuppies/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/hushpuppies/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/hushpuppies/static/media/slick.f97e3bbf.svg"
+  }
+]);
